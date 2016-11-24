3515f9b09a23e4d80bbe26a560c208f0a1646d5c
geobas <ksenera@yahoo.com>
2016-11-24 21:28:00 +0200

    Deploy bulletin board using node

--- build/.module-cache/c1c5cab9b7878697253039c369035170c6ca356e.js
+++
@@ -1,9 +0,0 @@
-var Hello = React.createClass({
-    displayName: "Hello",
-    render: function() {
-        return React.createElement("div", null,
-            React.createElement("h1", null, " Hello! "), " ", React.createElement("p", null, " This is some text "), " ")
-    }
-});
-React.render(React.createElement(Hello, null), document.body);
-// React.render(React.createElement('div', null, 'Hello!'), document.body);
