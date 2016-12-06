3f557afeb569806b26b4a867746d5fa1452857f5
geobas <ksenera@yahoo.com>
2016-12-06 23:16:46 +0200

    Setup karma & mocha for unit testing

---
+++ src/tests/Note-test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Note from '../Note.js';
+import TestUtils from 'react-addons-test-utils';
+import expect, { createSpy, spyOn, isSpy } from 'expect';
+
+describe('Note', function () {
+
+    it("renders a p", function () {
+        var note = TestUtils.renderIntoDocument(
+            <Note>I am a note!</Note>
+        );
+
+        var p = TestUtils.findRenderedDOMComponentWithTag(
+            note, 'p'
+		);
+
+		expect(p.textContent).toEqual("I am a note!");
+	});
+
+});
