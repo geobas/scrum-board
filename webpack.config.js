575cdd4f28ff8e72b81a050133b0b4bc26bbd646
geobas <ksenera@yahoo.com>
2017-08-16 20:38:51 +0300

    Various changes
    - Create a redux store to manage component's state
    - Replace grunt with webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+	entry: "./src/index-redux.js",
+
+	output: {
+		path: "dist/assets",
+		filename: "bundle.js",
+		publicPath: "assets"
+	},
+
+	devServer: {
+		inline: true,
+		contentBase: "./dist",
+		port: 3000
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: ['babel'],
+				query: {
+					presets: ['latest', 'stage-0']
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /(node_modules)/,
+				loader: 'json-loader'
+			}
+		]
+	}
+	
+}
